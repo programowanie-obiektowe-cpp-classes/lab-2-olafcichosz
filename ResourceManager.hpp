875a040deb180c108227439a9a972d58fb39adca
Olaf Cichosz <01171645@pw.edu.pl>
2024-11-03 11:13:39 +0100

    zaj_lab2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,46 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource; // wskaŸnik do zarz¹dzanego zasobu typu Resource
+
+public:
+    // Konstruktor domyœlny: tworzy nowy obiekt Resource
+    ResourceManager() : resource(new Resource()) {}
+
+    // Konstruktor kopiuj¹cy: tworzy kopiê zarz¹dzanego zasobu
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*other.resource)) {}
+
+    // Operator przypisania kopiuj¹cy
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            delete resource;                          // zwolnij obecny zasób
+            resource = new Resource(*other.resource); // skopiuj zasób z drugiego obiektu
+        }
+        return *this;
+    }
+
+    // Konstruktor przenosz¹cy
+    ResourceManager(ResourceManager&& other) noexcept : resource(other.resource)
+    {
+        other.resource = nullptr; // ustaw wskaŸnik w drugim obiekcie na nullptr
+    }
+
+    // Operator przypisania przenosz¹cy
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            delete resource;                 // zwolnij obecny zasób
+            resource       = other.resource; // przenieœ zasób
+            other.resource = nullptr;        // ustaw wskaŸnik w drugim obiekcie na nullptr
+        }
+        return *this;
+    }
+
+    // Destruktor: zwalnia zarz¹dzany zasób
+    ~ResourceManager() { delete resource; }
+
+    // Metoda get() zwracaj¹ca wynik wywo³ania get() zarz¹dzanego zasobu
+    double get() const { return resource->get(); }
 };
