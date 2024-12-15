c93941cd5cf61d720ce86d8dd1785f1bf2ad876b
Olaf Cichosz <01171645@pw.edu.pl>
2024-12-15 20:26:25 +0100

    juz

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,32 +5,27 @@
 class ResourceManager
 {
 private:
-    Resource* resource; // wskaŸnik do zarz¹dzanego zasobu typu Resource
+    Resource resource; // Zarz¹dzany zasób (bezpoœrednio jako pole klasy)
 
 public:
-    // Konstruktor domyœlny: tworzy nowy obiekt Resource
-    ResourceManager() : resource(new Resource()) {}
+    // Konstruktor domyœlny
+    ResourceManager() = default;
 
-    // Konstruktor kopiuj¹cy: tworzy kopiê zarz¹dzanego zasobu
-    ResourceManager(const ResourceManager& other) : resource(new Resource(*other.resource)) {}
+    // Konstruktor kopiuj¹cy
+    ResourceManager(const ResourceManager& other) = default;
 
     // Operator przypisania kopiuj¹cy
-    ResourceManager& operator=(const ResourceManager& other)
-    {
-        if (this != &other) {
-            delete resource;                          // zwolnij obecny zasób
-            resource = new Resource(*other.resource); // skopiuj zasób z drugiego obiektu
-        }
-        return *this;
-    }
+    ResourceManager& operator=(const ResourceManager& other) = default;
 
-    // Usuwamy semantykê przenoszenia
-    ResourceManager(ResourceManager&&) = delete;
-    ResourceManager& operator=(ResourceManager&&) = delete;
+    // Konstruktor przenosz¹cy
+    ResourceManager(ResourceManager&& other) noexcept = default;
 
-    // Destruktor: zwalnia zarz¹dzany zasób
-    ~ResourceManager() { delete resource; }
+    // Operator przypisania przenosz¹cy
+    ResourceManager& operator=(ResourceManager&& other) noexcept = default;
 
-    // Metoda get() zwracaj¹ca wynik wywo³ania get() zarz¹dzanego zasobu
-    double get() const { return resource->get(); }
+    // Destruktor
+    ~ResourceManager() = default;
+
+    // Metoda get() zwracaj¹ca wynik wywo³ania get() zasobu
+    double get() const { return resource.get(); }
 };
