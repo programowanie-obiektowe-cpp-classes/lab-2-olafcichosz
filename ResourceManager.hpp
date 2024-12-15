e4beecc5bc1563b4e0f9b16423b646a891ce87c2
Olaf Cichosz <01171645@pw.edu.pl>
2024-12-15 20:37:56 +0100

    zadanie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,31 +1,40 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <memory> // std::unique_ptr
 
 class ResourceManager
 {
 private:
-    Resource resource; // Zarz¹dzany zasób (bezpoœrednio jako pole klasy)
+    std::unique_ptr< Resource > resource; // Zarz¹dzany zasób (wskaŸnik unikalny)
 
 public:
-    // Konstruktor domyœlny
-    ResourceManager() = default;
+    // Konstruktor domyœlny: inicjalizuje nowy zasób
+    ResourceManager() : resource(std::make_unique< Resource >()) {}
 
-    // Konstruktor kopiuj¹cy
-    ResourceManager(const ResourceManager& other) = default;
+    // Konstruktor kopiuj¹cy: kopiuje zasób
+    ResourceManager(const ResourceManager& other)
+        : resource(std::make_unique< Resource >(*other.resource))
+    {}
 
     // Operator przypisania kopiuj¹cy
-    ResourceManager& operator=(const ResourceManager& other) = default;
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resource = std::make_unique< Resource >(*other.resource);
+        }
+        return *this;
+    }
 
-    // Konstruktor przenosz¹cy
+    // Konstruktor przenosz¹cy: przejmuje zasób
     ResourceManager(ResourceManager&& other) noexcept = default;
 
-    // Operator przypisania przenosz¹cy
+    // Operator przypisania przenosz¹cy: przejmuje zasób
     ResourceManager& operator=(ResourceManager&& other) noexcept = default;
 
-    // Destruktor
+    // Destruktor: automatycznie zwalnia zasób
     ~ResourceManager() = default;
 
-    // Metoda get() zwracaj¹ca wynik wywo³ania get() zasobu
-    double get() const { return resource.get(); }
+    // Metoda get() zwracaj¹ca wynik wywo³ania get() zarz¹dzanego zasobu
+    double get() const { return resource->get(); }
 };
